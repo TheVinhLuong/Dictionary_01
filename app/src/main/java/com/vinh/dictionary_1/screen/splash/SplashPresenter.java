8ef9f100be9e6e666d57d5dff0e81fceb73302d7
Vinh Luong <kurowanko68@gmail.com>
2017-11-01 10:01:43 +0700

    refs #6_code_refactoring

--- app/src/main/java/com/vinh/dictionary_1/screen/splash/SplashPresenter.java
+++ app/src/main/java/com/vinh/dictionary_1/screen/splash/SplashPresenter.java
@@ -41,7 +41,7 @@ final class SplashPresenter implements SplashContract.Presenter {
     private void assetHandler() {
         SharedPrefsImplement sharedPrefs = SharedPrefsImplement.getInstance();
         if (sharedPrefs.get(Constant.PREF_DICT_DB_COPIED, Boolean.class)) {
-            //TODO: Implement the db is copied's next scenario
+           //TODO move to home screen
         } else {
             mViewModel.showLoadingDialog(
                     MainApplication.getInstance().getString(R.string.msg_loading_please_wait));
